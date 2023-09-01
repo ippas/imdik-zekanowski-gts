42f8c2c1995ca26c794e9897a7e3d840c1a3047d
Malgorzata Borczyk <plggosborcz@login01.ares.cyfronet.pl>
2023-09-01 13:17:38 +0200

    new run of the model

--- preprocessing/oligogenic-model/oligogenic_model.py
+++ preprocessing/oligogenic-model/oligogenic_model.py
@@ -613,90 +613,94 @@ def model_assignment(results, genes):
 
 def prep_pl_for_skat(mts):
     
-    gtf = hl.import_table(
-        '/net/pr2/projects/plgrid/plggneuromol/resources/genecode_43/gencode.v43.basic.annotation.gtf',
-        delimiter = "\t",
-        no_header = True,
-        comment = "#"
-    )
-
-    # filter the gtf to only contain protein-coding genes with any GO term
-    gtf = gtf.filter((gtf.f2 == 'gene') & (gtf.f8.contains('protein_coding')))
-
-    gtf = gtf.select(
-        gene_position = hl.locus_interval(
-            gtf.f0,
-            hl.int(gtf.f3),
-            hl.int(gtf.f4),
-            reference_genome='GRCh38'
-        ),
-        gene_symbol = gtf.f8.split("\"")[5]
-    )  
-
-    go_genes = hl.import_table(
-        '/net/pr2/projects/plgrid/plggneuromol/resources/human-genes-with-GO-and-symbols'
-    )
-
-    go_genes = go_genes.key_by(go_genes['UniProtKB Gene Name symbol'])
-
-    gtf = gtf.key_by(gtf['gene_symbol'])
-    gtf = gtf.filter(
-        hl.is_defined(
-            go_genes[gtf.gene_symbol]
-        )
-    )
-
-    # overall we are keeping 18303 genes. At this poin the within gene intervals are extended by 20kb each side
-    start = hl.if_else(
-        gtf.gene_position.start.position <= 20000, 1, gtf.gene_position.start.position - 20000
-    )
-
-    contig_len = hl.contig_length(gtf.gene_position.start.contig, reference_genome='GRCh38') 
-
-    stop = hl.if_else(
-        (contig_len - gtf.gene_position.end.position) <= 20000,
-        contig_len,
-        gtf.gene_position.end.position + 20000
-    )
-
-    gtf = gtf.annotate(
-        interval_20kb =
-        hl.locus_interval(
-            gtf.gene_position.start.contig,
-            start,
-            stop,
-            reference_genome='GRCh38'
-        )
-    )
-
-    gtf = gtf.key_by(gtf.interval_20kb)
-
     for idx, mt in enumerate(mts):
        
-        if os.path.exists(localfs_path+'_pl_'+str(idx)+'annot.mt'):
+        if os.path.exists(localfs_path+'_pl_'+str(idx)+'ann.mt'):
             
-            mts[idx] = hl.read_matrix_table(localfs_path+'_pl_'+str(idx)+'annot.mt')
+            mts[idx] = hl.read_matrix_table(localfs_path+'_pl_'+str(idx)+'ann.mt')
             
         else:
-
-            mt = mt.annotate_rows(
-                    nearest_genes_20kb = hl.array(
-                        hl.set(
-                            gtf.index(mt.locus, all_matches=True)['gene_symbol'])
-                    )
-            )
-
-            mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
-            mt = mt.explode_rows(mt.nearest_genes_20kb)
-
-            mt = mt.filter_rows(mt.cadd.score_phred > 0)
-
-            if os.path.exists(localfs_path+'_'+str(idx)+'_temp.mt'):
-                mt = hl.read_matrix_table(localfs_path+'_'+str(idx)+'_temp.mt')
+            
+            if os.path.exists(localfs_path+'_'+str(idx)+'_tmp.mt'):
                 
-            else:
-                mt = mt.checkpoint(localfs_path+'_'+str(idx)+'_temp.mt')
+                mt = hl.read_matrix_table(localfs_path+'_'+str(idx)+'_tmp.mt')
+                
+            else:   
+    
+                gtf = hl.import_table(
+                    '/net/pr2/projects/plgrid/plggneuromol/resources/genecode_43/gencode.v43.basic.annotation.gtf',
+                    delimiter = "\t",
+                    no_header = True,
+                    comment = "#"
+                )
 
+                # filter the gtf to only contain protein-coding genes with any GO term
+                gtf = gtf.filter((gtf.f2 == 'gene') & (gtf.f8.contains('protein_coding')))
+
+                gtf = gtf.select(
+                    gene_position = hl.locus_interval(
+                        gtf.f0,
+                        hl.int(gtf.f3),
+                        hl.int(gtf.f4),
+                        reference_genome='GRCh38'
+                    ),
+                    gene_symbol = gtf.f8.split("\"")[5]
+                )  
+
+                go_genes = hl.import_table(
+                    '/net/pr2/projects/plgrid/plggneuromol/resources/human-genes-with-GO-and-symbols'
+                )
+
+                go_genes = go_genes.key_by(go_genes['UniProtKB Gene Name symbol'])
+
+                gtf = gtf.key_by(gtf['gene_symbol'])
+                gtf = gtf.filter(
+                    hl.is_defined(
+                        go_genes[gtf.gene_symbol]
+                    )
+                )
+
+                # overall we are keeping 18303 genes. At this poin the within gene intervals are extended by 20kb each side
+                start = hl.if_else(
+                    gtf.gene_position.start.position <= 20000, 1, gtf.gene_position.start.position - 20000
+                )
+
+                contig_len = hl.contig_length(gtf.gene_position.start.contig, reference_genome='GRCh38') 
+
+                stop = hl.if_else(
+                    (contig_len - gtf.gene_position.end.position) <= 20000,
+                    contig_len,
+                    gtf.gene_position.end.position + 20000
+                )
+
+                gtf = gtf.annotate(
+                    interval_20kb =
+                    hl.locus_interval(
+                        gtf.gene_position.start.contig,
+                        start,
+                        stop,
+                        reference_genome='GRCh38'
+                    )
+                )
+
+                gtf = gtf.key_by(gtf.interval_20kb)
+
+                mt = mt.annotate_rows(
+                        nearest_genes_20kb = hl.array(
+                            hl.set(
+                                gtf.index(mt.locus, all_matches=True)['gene_symbol'])
+                        )
+                )
+
+                mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
+                mt = mt.explode_rows(mt.nearest_genes_20kb)
+
+                mt = mt.filter_rows(mt.cadd.score_phred > 0)
+                mt = mt.naive_coalesce(100)
+
+                mt = mt.checkpoint(localfs_path+'_'+str(idx)+'_tmp.mt')
+
+            
             mt = mt.annotate_cols(
                 sex = hl.if_else(
                     mt.group == 'GTS',
@@ -705,14 +709,11 @@ def prep_pl_for_skat(mts):
                 )
             )
 
-
-            mt = mt.checkpoint(localfs_path+'_pl_'+str(idx)+'annot.mt')
+            mt = mt.checkpoint(localfs_path+'_pl_'+str(idx)+'ann.mt')
 
             mts[idx] = mt
     
     return(mts)
-    
-
 
 def run_skat_log(mtx, pcs, field, suffix):
     
@@ -748,54 +749,13 @@ def run_skat_for_mts(mt_list):
 
         for pc in list(range(21)):
             
-            print('running with '+str(pc)+' PCs and the pruned scores option')
-            
-            if os.path.exists(localfs_path+'skat_table_'+str(pc)+'_scores_part_'+str(idx)+'.mt'):
-            
-                skat_table = hl.read_table(
-                        localfs_path+'skat_table_'+str(pc)+'_scores_part_'+str(idx)+'.mt'
-                    )
-                
-                skat_tables.append(skat_table)
-                
-                plots.append(
-                    hl.plot.qq(
-                        skat_table.p_value
-                    )
-                )
-                
-                options.append(
-                        'PCs '+str(pc)+' pruned scores subset '+str(idx)
-                    )
-                
-            else:
-                
-                try:
-                    skat_table, qq_plot = run_skat_log(
-                            mtx,
-                            pc,
-                            'pruned_scores',
-                            '_part_'+str(idx)
-                    )
-
-                    skat_tables.append(skat_table)
-                    plots.append(qq_plot)
-
-                    options.append(
-                            'PCs '+str(pc)+' pruned scores'+'eur subset '+str(idx)
-                        )
-
-                except Exception as e:
-                    print('failed PCs '+str(pc)+' pruned scores'+'eur subset '+str(idx))
-                    print(type(e))
-                
 
             print('running with '+str(pc)+' PCs and the just subsetet scores')
             
-            if os.path.exists(localfs_path+'skat_table_'+str(pc)+'_scores_no_filter_part_'+str(idx)+'.mt'):
+            if os.path.exists(localfs_path+'skat_table_'+str(pc)+'_scores_no_filter_parts_'+str(idx)+'.mt'):
             
                 skat_table = hl.read_table(
-                    localfs_path+'skat_table_'+str(pc)+'_scores_no_filter_part_'+str(idx)+'.mt'
+                    localfs_path+'skat_table_'+str(pc)+'_scores_no_filter_parts_'+str(idx)+'.mt'
                 )
                 
                 skat_tables.append(skat_table)
@@ -803,6 +763,7 @@ def run_skat_for_mts(mt_list):
                 plots.append(hl.plot.qq(
                     skat_table.p_value
                 ))
+                
                 options.append(
                         'PCs '+str(pc)+' subseted scores subset '+str(idx)
                     )
@@ -813,17 +774,18 @@ def run_skat_for_mts(mt_list):
                         mtx,
                         pc,
                         'scores_no_filter',
-                        '_part_'+str(idx)
+                        '_parts_'+str(idx)
                     )
 
                     skat_tables.append(skat_table_no_filt)
                     plots.append(qq_plot_no_filt)
+                    
                     options.append(
                         'PCs '+str(pc)+' no filters subseted scores subset '+str(idx)
                     )
 
                 except Exception as e:
-                    print('failed PCs '+str(pc)+' no filters subseted scores'+'eur subset '+str(idx))
+                    print('failed PCs '+str(pc)+' no filters subseted scores'+'subset '+str(idx))
                     print(type(e))
             
     return(skat_tables, plots, options)
